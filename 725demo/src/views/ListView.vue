be6b73b709d7a3b4b90b54e52c39f97f39ec1bf6
Daniel Meilech <melikhov@uw.edu>
2023-07-26 18:42:16 -0700

    Fixed Course View and Lecture View navbar names + debugged noth pages visually

--- 725demo/src/views/ListView.vue
+++ 725demo/src/views/ListView.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="container">
-    <div class="left-box">
-      <div class="left-box-top">
+    <div class="box left-box">
+      <div class="image-container">
         <img src="../assets/listview1.png" alt="">
       </div>
-      <div class="left-box-bottom">
+      <div class="content-container">
         <h3>Medical 101</h3>
         <h4>Course Description</h4>
-        <p style="margin: 0; line-height: 25px;">in this course, vou will learn the foundations of medical sciences.
-          Wewill cover topics such as anatomy,
-          medical terminology, and clinicaskills. This course is designed to provide you with a solidunderstanding of the
-          medical field.</p>
+        <p class="description-text">in this course, you will learn the foundations of medical sciences. We will cover topics such as anatomy, medical terminology, and clinical skills. This course is designed to provide you with a solid understanding of the medical field.</p>
       </div>
     </div>
-    <div class="right-box">
-      <div class="right-box-content">
+    <div class="box right-box">
+      <div class="content-container">
         <h3>Course Overview</h3>
         <div class="right-box-item" v-for="(item,index) in arr" :key="index">
-          <div style="margin-left: 20px;">{{item}}</div>
+          <div>{{item}}</div>
           <div class="arrow-right"> > </div>
         </div>
         <div class="btn">Enroll now</div>
@@ -33,83 +30,68 @@ export default {
       arr: ['Study Guide','Text Books','Lecture 1','Lecture 2','Lecture 3']
     }
   },
-
 }
 </script>
 
 <style lang="less" scoped>
 .container {
-  background: #ececec;
   display: flex;
   justify-content: space-between;
+  align-items: stretch; 
+  background: #ececec;
+  height: 100vh; 
+}
+
+.box {
+  flex: 1;
+  background: #fff;
+  display: flex;
+  flex-direction: column; 
+}
+
+.image-container {
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+
+.content-container {
+  padding: 20px;
+
+  .description-text {
+    margin: 0; 
+    line-height: 25px;
+    word-wrap: break-word;
+  }
+}
+
+.right-box-item {
+  display: flex;
+  justify-content: space-between;
+  background: #c2d7ef;
+  margin: 10px 0;
+  padding: 20px;
   align-items: center;
-
-  .left-box {
-    width: 47%;
-    height: 100%;
-    background: #fff;
-
-    .left-box-top {
-      width: 650px;
-      height: 380px;
-      margin: 50px auto;
-
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-
-    .left-box-bottom {
-      width: 650px;
-      margin: 50px auto;
-      text-align: left;
-    }
+  .arrow-right {
+    margin-right: 20px;
+    cursor: pointer;
   }
+}
 
-  .right-box {
-    width: 47%;
-    height: 100%;
+.right-box {
+  background: #ececec;
+}
 
-    .right-box-content {
-      margin: 20px auto;
-      width: 80%;
-      height: 800px;
-      text-align: left;
-      .btn{
-        width: 100%;
-        height: 45px;
-        line-height: 45px;
-        color: #fff;
-        background: #476b9a;
-        font-weight: bold;
-        text-align: center;
-        cursor: pointer;
-        margin-top: 200px;
-      }
-    }
-    .right-box-item{
-      height: 80px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      background: #c2d7ef;
-      line-height: 80px;
-      margin: 10px 0;
-      font-weight: bold;
-      .arrow-right{
-        width: 50px;
-        height: 50px;
-        line-height: 50px;
-        text-align: center;
-        color: #000;
-        border: 1px solid #000;
-        border-radius: 3px;
-        cursor: pointer;
-        margin-right: 20px;
-      }
-    }
-
-  }
+.btn {
+  display: inline-block;
+  width: 100%;
+  line-height: 45px;
+  margin-top: 20px;
+  background: #476b9a;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+  cursor: pointer;
 }
 </style>
--- 725demo/src/views/ListView2.vue
+++ 725demo/src/views/ListView2.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="container">
-    <div class="left-box">
-      <div class="left-box-top">
+    <div class="box left-box">
+      <div class="image-container">
         <img src="../assets/listview2.png" alt="">
       </div>
-      <div class="left-box-bottom">
+      <div class="content-container">
         <h3>Lecturel</h3>
         <h4>Full Lecture Transcript</h4>
-        <p style="margin: 0; line-height: 25px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        <p class="description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation
           ullamco laboris nisiut aliquip ex ea commodo consequat, Duis gute irure dolor inreprehenderit in voluptate velit
           esse cillum dolore eu fugiat nullopariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui
           officia deserunt mollit anim id est laborum.</p>
-        <div class="right-box-item" style="margin-top: 50px;">
-          <div style="margin-left: 20px;">Lecture Handout</div>
+        <div class="right-box-item">
+          <div>Lecture Handout</div>
           <div class="arrow-right"> > </div>
         </div>
       </div>
     </div>
-    <div class="right-box">
-      <div class="right-box-content">
+    <div class="box right-box">
+      <div class="content-container">
         <h3>Lecture Materials</h3>
         <div class="right-box-item" v-for="(item, index) in arr" :key="index">
-          <div style="margin-left: 20px;">{{ item }}</div>
+          <div>{{ item }}</div>
           <div class="arrow-right"> > </div>
         </div>
         <div class="btn">Enroll now</div>
@@ -38,85 +38,68 @@ export default {
       arr: ['Lecture Summary', 'Handout Summary', 'Study Cards']
     }
   },
-
 }
 </script>
 
 <style lang="less" scoped>
 .container {
-  background: #ececec;
   display: flex;
   justify-content: space-between;
+  align-items: stretch; 
+  background: #ececec;
+  height: 100vh; 
+}
+
+.box {
+  flex: 1;
+  background: #fff;
+  display: flex;
+  flex-direction: column; 
+}
+
+.image-container {
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+
+.content-container {
+  padding: 20px;
+
+  .description-text {
+    margin: 0; 
+    line-height: 25px;
+    word-wrap: break-word;
+  }
+}
+
+.right-box-item {
+  display: flex;
+  justify-content: space-between;
+  background: #c2d7ef;
+  margin: 10px 0;
+  padding: 20px;
   align-items: center;
-
-  .left-box {
-    width: 47%;
-    height: 100%;
-    background: #fff;
-
-    .left-box-top {
-      width: 650px;
-      height: 380px;
-      margin: 50px auto;
-
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-
-    .left-box-bottom {
-      width: 650px;
-      margin: 50px auto;
-      text-align: left;
-    }
+  .arrow-right {
+    margin-right: 20px;
+    cursor: pointer;
   }
+}
 
-  .right-box {
-    width: 47%;
-    height: 100%;
+.right-box {
+  background: #ececec;
+}
 
-    .right-box-content {
-      margin: 20px auto;
-      width: 80%;
-      height: 800px;
-      text-align: left;
-
-      .btn {
-        width: 100%;
-        height: 45px;
-        line-height: 45px;
-        color: #fff;
-        background: #476b9a;
-        font-weight: bold;
-        text-align: center;
-        cursor: pointer;
-        margin-top: 450px;
-      }
-    }
-  }
-
-  .right-box-item {
-    height: 80px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background: #c2d7ef;
-    line-height: 80px;
-    margin: 10px 0;
-    font-weight: bold;
-
-    .arrow-right {
-      width: 50px;
-      height: 50px;
-      line-height: 50px;
-      text-align: center;
-      color: #000;
-      border: 1px solid #000;
-      border-radius: 3px;
-      cursor: pointer;
-      margin-right: 20px;
-    }
-  }
+.btn {
+  display: inline-block;
+  width: 100%;
+  line-height: 45px;
+  margin-top: 20px;
+  background: #476b9a;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+  cursor: pointer;
 }
 </style>
