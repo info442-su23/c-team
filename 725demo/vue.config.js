11352ef008357b6f085da935f3992a433789b3b4
Daniel Meilech <melikhov@uw.edu>
2023-07-26 23:44:24 -0700

    Deployment attempt 3

--- 725demo/vue.config.js
+++ 725demo/vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/c-team/725demo/'
-    : '/',
   transpileDependencies: true
 })
